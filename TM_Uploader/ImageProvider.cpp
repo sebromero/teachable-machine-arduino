b7b2df67980bd018639b4aedda58633cd426df72
Sebastian Romero <s.romero@arduino.cc>
2025-05-07 12:54:20 +0200

    Refactor image processing code

--- TM_Uploader/ImageProvider.cpp
+++ TM_Uploader/ImageProvider.cpp
@@ -81,80 +81,70 @@ bool cameraBegin() {
   #endif
 }
 
-// Crop image and convert it to grayscale
-boolean processImage(
-  int image_width, int image_height,
-  uint8_t* image_data) {
-  //  Serial.println("begging image process");
-  //  const int skip_start_x = ceil((kCaptureWidth - image_width) / 2); // 40.5
-  //  const int skip_start_y = ceil((kCaptureHeight - image_height) / 2); // 24
-  //  const int skip_end_x_index = (kCaptureWidth - skip_start_x); // (176 - 40) = 135
-  //  const int skip_end_y_index = (kCaptureHeight - skip_start_y); // 144 - 24 = 120
-  const int imgSize = 96;
+// Convert RGB565 color to grayscale value
+float convertRGB565ToGrayscale(uint16_t color) {
+  // Extract the color values (5 red bits, 6 green, 5 blue)
+  uint8_t r = ((color & 0xF800) >> 11) * 8;
+  uint8_t g = ((color & 0x07E0) >> 5) * 4;
+  uint8_t b = ((color & 0x001F) >> 0) * 8;
+  
+  // Convert to grayscale by calculating luminance
+  // See https://en.wikipedia.org/wiki/Grayscale for magic numbers
+  return (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
+}
 
-  // Color of the current pixel
-  uint16_t color;
+// Read RGB565 color from buffer at specified index
+uint16_t readColorFromBuffer(const uint8_t* buffer, int index) {
+  if (index < 0 || index >= capDataLen - 1) {
+    return 0; // Return black if out of bounds
+  }
+  
+  uint8_t high_byte = buffer[index];
+  uint8_t low_byte = buffer[index + 1];
+  return ((uint16_t)high_byte << 8) | low_byte;
+}
+
+// Crop image and convert it to grayscale
+boolean processImage(int image_width, int image_height, uint8_t* image_data) {
+  const int imgSize = 96;
+  
   for (int y = 0; y < imgSize; y++) {
     for (int x = 0; x < imgSize; x++) {
+      // Map destination coordinates to source coordinates for cropping
       int currentCapX = floor(map(x, 0, imgSize, 40, kCaptureWidth - 80));
       int currentCapY = floor(map(y, 0, imgSize, 0, kCaptureHeight));
-      // Read the color of the pixel as 16-bit integer
-      int read_index = (currentCapY * kCaptureWidth + currentCapX) * 2;//(y * kCaptureWidth + x) * 2;
-      int i2 = (currentCapY * kCaptureWidth + currentCapX + 1) * 2;
-      int i3 = ((currentCapY + 1) * kCaptureWidth + currentCapX) * 2;
-      int i4 = ((currentCapY + 1) * kCaptureWidth + currentCapX + 1) * 2;
-
-      uint8_t high_byte = captured_data[read_index];
-      uint8_t low_byte = captured_data[read_index + 1];
-
-      color = ((uint16_t)high_byte << 8) | low_byte;
-      // Extract the color values (5 red bits, 6 green, 5 blue)
-      uint8_t r, g, b;
-      r = ((color & 0xF800) >> 11) * 8;
-      g = ((color & 0x07E0) >> 5) * 4;
-      b = ((color & 0x001F) >> 0) * 8;
-      // Convert to grayscale by calculating luminance
-      // See https://en.wikipedia.org/wiki/Grayscale for magic numbers
-      float gray_value = (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
-
-      if (i2 > 0 && i2 < capDataLen - 1) {
-        high_byte = captured_data[i2];
-        low_byte = captured_data[i2 + 1];
-        color = ((uint16_t)high_byte << 8) | low_byte;
-        r = ((color & 0xF800) >> 11) * 8;
-        g = ((color & 0x07E0) >> 5) * 4;
-        b = ((color & 0x001F) >> 0) * 8;
-        gray_value += (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
+      
+      // Calculate indices for 2x2 pixel sampling
+      int indices[4] = {
+        (currentCapY * kCaptureWidth + currentCapX) * 2,                   // Top-left
+        (currentCapY * kCaptureWidth + currentCapX + 1) * 2,               // Top-right
+        ((currentCapY + 1) * kCaptureWidth + currentCapX) * 2,             // Bottom-left
+        ((currentCapY + 1) * kCaptureWidth + currentCapX + 1) * 2          // Bottom-right
+      };
+      
+      // Sample 2x2 grid of pixels and average their grayscale values
+      float gray_value = 0.0f;
+      int valid_samples = 0;
+      
+      for (int i = 0; i < 4; i++) {
+        if (indices[i] >= 0 && indices[i] < capDataLen - 1) {
+          uint16_t color = readColorFromBuffer(captured_data, indices[i]);
+          gray_value += convertRGB565ToGrayscale(color);
+          valid_samples++;
+        }
       }
-      if (i3 > 0 && i3 < capDataLen - 1) {
-        high_byte = captured_data[i3];
-        low_byte = captured_data[i3 + 1];
-        color = ((uint16_t)high_byte << 8) | low_byte;
-        r = ((color & 0xF800) >> 11) * 8;
-        g = ((color & 0x07E0) >> 5) * 4;
-        b = ((color & 0x001F) >> 0) * 8;
-        gray_value += (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
+      
+      // Average the grayscale values from valid samples
+      if (valid_samples > 0) {
+        gray_value /= valid_samples;
       }
-
-      if (i4 > 0 && i4 < capDataLen - 1) {
-        high_byte = captured_data[i4];
-        low_byte = captured_data[i4 + 1];
-        color = ((uint16_t)high_byte << 8) | low_byte;
-        r = ((color & 0xF800) >> 11) * 8;
-        g = ((color & 0x07E0) >> 5) * 4;
-        b = ((color & 0x001F) >> 0) * 8;
-        gray_value += (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
-      }
-
-      gray_value = gray_value / 4;
-
-      // Convert to signed 8-bit integer by subtracting 128.
-      //
-      //      // The index of this pixel` in our flat output buffer
-      int index = y * image_width + x;
-      image_data[index] = static_cast<int8_t>(gray_value);
+      
+      // The index of this pixel in our flat output buffer
+      int output_index = y * image_width + x;
+      image_data[output_index] = static_cast<int8_t>(gray_value);
     }
   }
+  
   return true;
 }
 
